c2f335b31ee39ff6efb65c21cc13ba5e95d9302d
xsetra <abdullahcaliskan511@gmail.com>
2018-12-16 13:42:55 +0300

    getting Turkish airline reviews

--- tripadvisor/tripadvisor/spiders/airlineSpider.py
+++ tripadvisor/tripadvisor/spiders/airlineSpider.py
@@ -4,4 +4,15 @@ import scrapy
 class AirlineSpider(scrapy.Spider):
     name = "AirlineSpider"
 
-    
+    def start_requests(self):
+        urls = [
+            'https://www.tripadvisor.com.tr/Airline_Review-d8729174-Reviews-Turkish-Airlines',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        filename = 'turkish-airlines.html'
+        with open(filename, 'wb') as fp:
+            fp.write(response.body)
+        self.log('Saved file {}'.format(filename))
