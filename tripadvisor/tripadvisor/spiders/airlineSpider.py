cc43abd98f1ea7e57f3b387f7dffa5fa41eb4385
xsetra <abdullahcaliskan511@gmail.com>
2018-12-16 16:32:00 +0300

    Airline Reviews getted for all the pages.

--- tripadvisor/tripadvisor/spiders/airlineSpider.py
+++ tripadvisor/tripadvisor/spiders/airlineSpider.py
@@ -12,13 +12,14 @@ class AirlineSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        titles = response.css('span.noQuotes::text').extract()
-        entries = response.css('div.entry p.partial_entry::text').extract()
-        rates = response.css('div.innerBubble div.wrap div.reviewItemInline span.ui_bubble_rating').extract()
-        return {
-            'page_1': {
-                'titles': titles,
-                'entries': entries,
-                'rates': rates
+        for review in response.css('div.review_collection div.reviewSelector'):
+            yield {
+                'title': review.css('span.noQuotes::text').extract_first(),
+                'entry': review.css('div.entry p.partial_entry::text').extract_first(),
+                'rate': review.css('div.reviewItemInline span.ui_bubble_rating').extract_first(),
             }
-        }
+
+        next_page = response.css('a.nav.next::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page, callback=self.parse)
